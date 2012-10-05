e2ed7ea5b4b0e7264241461b550bc24f7639922b
Relados <blagh0@yahoo.com>
2012-10-05 20:38:13 -0300

    adding a lot more gen3 mechanics

--- Relados-OM-Server-for-PS/Relados-OM-Server-for-PS/Relados/mods/gen3/abilities.js
+++ Relados-OM-Server-for-PS/Relados-OM-Server-for-PS/Relados/mods/gen3/abilities.js
@@ -1,4 +1,52 @@
 exports.BattleAbilities = {
+	"cutecharm": {
+		desc: "If an opponent of the opposite gender directly attacks this Pokemon, there is a 30% chance that the opponent will become Attracted to this Pokemon.",
+		shortDesc: "30% chance of infatuating Pokemon of the opposite gender if they make contact.",
+		onAfterDamage: function(damage, target, source, move) {
+			if (move && move.isContact) {
+				if (this.random(4) < 1) {
+					if (source.addVolatile('attract', target)) {
+						this.add('-start', source, 'Attract', '[from] Cute Charm', '[of] '+target);
+					}
+				}
+			}
+		},
+		id: "cutecharm",
+		name: "Cute Charm",
+		rating: 2,
+		num: 56
+	},
+	"effectspore": {
+		desc: "If an opponent directly attacks this Pokemon, there is a 30% chance that the opponent will become either poisoned, paralyzed or put to sleep. There is an equal chance to inflict each status.",
+		shortDesc: "30% chance of poisoning, paralyzing, or causing sleep on Pokemon making contact.",
+		onAfterDamage: function(damage, target, source, move) {
+			if (move && move.isContact && !source.status) {
+				var r = this.random(300);
+				if (r < 10) source.setStatus('slp');
+				else if (r < 20) source.setStatus('par');
+				else if (r < 30) source.setStatus('psn');
+			}
+		},
+		id: "effectspore",
+		name: "Effect Spore",
+		rating: 2,
+		num: 27
+	},
+	"flamebody": {
+		desc: "If an opponent directly attacks this Pokemon, there is a 30% chance that the opponent will become burned.",
+		shortDesc: "30% chance of burning a Pokemon making contact with this Pokemon.",
+		onAfterDamage: function(damage, target, source, move) {
+			if (move && move.isContact) {
+				if (this.random(4) < 1) {
+					source.trySetStatus('brn', target, move);
+				}
+			}
+		},
+		id: "flamebody",
+		name: "Flame Body",
+		rating: 2,
+		num: 49
+	},
 	"flashfire": {
 		desc: "This Pokemon is immune to all Fire-type attacks; additionally, its own Fire-type attacks receive a 50% boost if a Fire-type move hits this Pokemon. Multiple boosts do not occur if this Pokemon is hit with multiple Fire-type attacks.",
 		shortDesc: "This Pokemon's Fire attacks do 1.5x damage if hit by one Fire move; Fire immunity.",
@@ -42,6 +90,21 @@ exports.BattleAbilities = {
 		rating: 0,
 		num: 1
 	},
+	"poisonpoint": {
+		desc: "If an opponent directly attacks this Pokemon, there is a 30% chance that the opponent will become poisoned.",
+		shortDesc: "30% chance of poisoning a Pokemon making contact with this Pokemon.",
+		onAfterDamage: function(damage, target, source, move) {
+			if (move && move.isContact) {
+				if (this.random(4) < 1) {
+					source.trySetStatus('psn', target, move);
+				}
+			}
+		},
+		id: "poisonpoint",
+		name: "Poison Point",
+		rating: 2,
+		num: 38
+	},
 	"pressure": {
 		desc: "When an opponent uses a move that affects this Pokemon, an additional PP is required for the opponent to use that move.",
 		shortDesc: "If this Pokemon is the target of a move, that move loses one additional PP.",
@@ -53,6 +116,17 @@ exports.BattleAbilities = {
 		name: "Pressure",
 		rating: 1,
 		num: 46
+	},
+	"rockhead": {
+		desc: "This Pokemon does not receive recoil damage unless it uses Struggle, it misses with Jump Kick or Hi Jump Kick or it is holding Life Orb, Jaboca Berry or Rowap Berry.",
+		shortDesc: "This Pokemon does not take recoil damage besides Struggle, Life Orb, crash damage.",
+		onModifyMove: function(move) {
+			if (move.id !== 'struggle') delete move.recoil;
+		},
+		id: "rockhead",
+		name: "Rock Head",
+		rating: 3.5,
+		num: 69
 	},
 	"roughskin": {
 		desc: "Causes recoil damage equal to 1/16 of the opponent's max HP if an opponent directly attacks.",
@@ -77,6 +151,21 @@ exports.BattleAbilities = {
 		name: "Shadow Tag",
 		rating: 5,
 		num: 23
+	},
+	"static": {
+		desc: "If an opponent directly attacks this Pokemon, there is a 30% chance that the opponent will become paralyzed.",
+		shortDesc: "30% chance of paralyzing a Pokemon making contact with this Pokemon.",
+		onAfterDamage: function(damage, target, source, effect) {
+			if (effect && effect.isContact) {
+				if (this.random(4) < 1) {
+					source.trySetStatus('par', target, effect);
+				}
+			}
+		},
+		id: "static",
+		name: "Static",
+		rating: 2,
+		num: 9
 	},
 	"stench": {
 		desc: "No in-battle effect.",
@@ -109,27 +198,6 @@ exports.BattleAbilities = {
 			if (status === 'slp' || status === 'frz') return;
 			if (status === 'tox') status = 'psn';
 			source.trySetStatus(status);
-		}
-	},
-	"trace": {
-		inherit: true,
-		onUpdate: function(pokemon) {
-			var target = pokemon.side.foe.randomActive();
-			if (!target || target.fainted) return;
-			var ability = this.getAbility(target.ability);
-			var bannedAbilities = {forecast:1, multitype:1, trace:1};
-			if (bannedAbilities[target.ability]) {
-				return;
-			}
-			if (ability === 'Intimidate')
-			{
-				if (pokemon.setAbility('Illuminate')) {  //Temporary fix so Intimidate doesn't activate in third gen when traced
-					this.add('-ability',pokemon, ability,'[from] ability: Trace','[of] '+target);
-				}
-			}
-			else if (pokemon.setAbility(ability)) {
-				this.add('-ability',pokemon, ability,'[from] ability: Trace','[of] '+target);
-			}
 		}
 	},
 	"wonderguard": {
